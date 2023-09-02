83b13117e67f85fa7896882d52cb51bfe09d9d0e
Viswak <madupuru@gmail.com>
2023-09-02 17:53:38 -0400

    state file updated

--- terraform.tf
+++ terraform.tf
@@ -5,11 +5,11 @@ terraform {
       version = "5.13.0"
     }
   }
-  backend "s3" {
-    bucket = "viswaterraformbucket-statefile/terraform-statefile/"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
+  # backend "s3" {
+  #   bucket = "viswaterraformbucket-statefile/terraform-statefile/"
+  #   key    = "terraform.tfstate"
+  #   region = "us-east-1"
+  # }
 }
 provider "aws" {
   region  = "us-east-1"
